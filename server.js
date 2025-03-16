b95e15386ce980622d944d991800ffe60f73ec0f
Bose <bosebeast@gmail.com>
2025-03-16 23:17:34 +0530

    server babe

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://team-imperium.netlify.app/"],
+    origin: ["https://team-imperium.netlify.app/contact"],
     credentials: true,
   })
 );
